7fa8e2257801ed54add715d881e8aad82dde9663
matheusrodacki <44122620+matheusrodacki@users.noreply.github.com>
2023-09-12 13:16:11 -0300

    Add endpoint to retrieve a specific person
    - Added a new endpoint in the `PessoaController` class to retrieve information about a specific person.
    - The new endpoint can be accessed at `/pessoas/:id` using a GET request.
    - The endpoint uses the `pegaUmaPessoa` method to fetch the person's data from the database.
    - If successful, the person's information is returned in the response with a 200 status code.
    - If an error occurs, a 500 status code is returned along with the error message.

--- api/controllers/PessoaController.js
+++ api/controllers/PessoaController.js
@@ -9,6 +9,17 @@ class PessoaController {
       return res.status(500).json(erro.message);
     }
   }
+  static async pegaUmaPessoa(req, res) {
+    const { id } = req.params;
+    try {
+      const pessoa = await database.Pessoas.findOne({
+        where: { id: Number(id) },
+      });
+      return res.status(200).json(pessoa);
+    } catch (error) {
+      return res.status(500).json(erro.message);
+    }
+  }
 }
 
 module.exports = PessoaController;
