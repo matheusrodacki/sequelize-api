87f52f4231001cfe674e1b11ba085e0c0457c187
matheusrodacki <44122620+matheusrodacki@users.noreply.github.com>
2023-09-13 05:59:33 -0300

    feat: Add create and update routes for Pessoa
    - Add a new route to create a new Pessoa
    - Add a new route to update an existing Pessoa by ID

--- api/controllers/PessoaController.js
+++ api/controllers/PessoaController.js
@@ -13,9 +13,33 @@ class PessoaController {
     const { id } = req.params;
     try {
       const pessoa = await database.Pessoas.findOne({
-        where: { id: Number(id) },
+        where: {
+          id: Number(id),
+        },
       });
       return res.status(200).json(pessoa);
+    } catch (error) {
+      return res.status(500).json(erro.message);
+    }
+  }
+  static async criaPessoa(req, res) {
+    const novaPessoa = req.body;
+    try {
+      const novaPessoaCriada = await database.Pessoas.create(novaPessoa);
+      return res.status(200).json(novaPessoaCriada);
+    } catch (error) {
+      return res.status(500).json(erro.message);
+    }
+  }
+  static async atualizaPessoa(req, res) {
+    const { id } = req.params;
+    const novasInfos = req.body;
+    try {
+      await database.Pessoas.update(novasInfos, { where: { id: Number(id) } });
+      const pessoaAtualizada = await database.Pessoas.findOne({
+        where: { id: Number(id) },
+      });
+      return res.status(200).json(pessoaAtualizada);
     } catch (error) {
       return res.status(500).json(erro.message);
     }
