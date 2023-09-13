8c8dd21c9eb2672e462ca90cfecdbf05124f9451
matheusrodacki <44122620+matheusrodacki@users.noreply.github.com>
2023-09-13 06:45:43 -0300

    Refactor PessoaController and add delete functionality - Added a new static method `deletaPessoa` to the `PessoaController` class that handles the delete request of a person by their id. - Updated `pessoasRoute.js` to include the new delete route.

--- api/controllers/PessoaController.js
+++ api/controllers/PessoaController.js
@@ -44,6 +44,15 @@ class PessoaController {
       return res.status(500).json(erro.message);
     }
   }
+  static async deletaPessoa(req, res) {
+    const { id } = req.params;
+    try {
+      await database.Pessoas.destroy({ where: { id: Number(id) } });
+      return res.status(200).json({ mensage: `id ${id} deletado` });
+    } catch (error) {
+      return res.status(500).json(erro.message);
+    }
+  }
 }
 
 module.exports = PessoaController;
--- api/routes/pessoasRoute.js
+++ api/routes/pessoasRoute.js
@@ -7,5 +7,6 @@ router.get("/pessoas", PessoaController.pegaTodasAsPessoas);
 router.get("/pessoas/:id", PessoaController.pegaUmaPessoa);
 router.post("/pessoas", PessoaController.criaPessoa);
 router.put("/pessoas/:id", PessoaController.atualizaPessoa);
+router.delete("/pessoas/:id", PessoaController.deletaPessoa);
 
 module.exports = router;
