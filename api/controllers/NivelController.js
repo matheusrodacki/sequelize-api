b35af7712202f88b869e2951caa2736682188de0
matheusrodacki <44122620+matheusrodacki@users.noreply.github.com>
2023-09-14 06:08:46 -0300

    Add NivelController and TurmaController with their respective routes
    - Add NivelController class with methods to retrieve, create, update and delete levels
    - Add TurmaController class with methods to retrieve, create, update and delete classes
    - Add routes for NivelController and TurmaController in the index.js file of the routes folder.

---
+++ api/controllers/NivelController.js
@@ -0,0 +1,60 @@
+const database = require("../models");
+
+//controllers/NivelController.js
+
+class NivelController {
+  static async pegaTodosOsNiveis(req, res) {
+    try {
+      const todosOsNiveis = await database.Niveis.findAll();
+      return res.status(200).json(todosOsNiveis);
+    } catch (error) {
+      return res.status(500).json(error.message);
+    }
+  }
+  static async pegaUmNivel(req, res) {
+    const { id } = req.params;
+    try {
+      const nivel = await database.Niveis.findOne({
+        where: {
+          id: Number(id),
+        },
+      });
+      return res.status(200).json(nivel);
+    } catch (error) {
+      return res.status(500).json(erro.message);
+    }
+  }
+  static async criaNivel(req, res) {
+    const novoNivel = req.body;
+    try {
+      const novoNivelCriado = await database.Niveis.create(novoNivel);
+      return res.status(200).json(novoNivelCriado);
+    } catch (error) {
+      return res.status(500).json(erro.message);
+    }
+  }
+  static async atualizaNivel(req, res) {
+    const { id } = req.params;
+    const novasInfos = req.body;
+    try {
+      await database.Niveis.update(novasInfos, { where: { id: Number(id) } });
+      const nivelAtualizado = await database.Niveis.findOne({
+        where: { id: Number(id) },
+      });
+      return res.status(200).json(nivelAtualizado);
+    } catch (error) {
+      return res.status(500).json(erro.message);
+    }
+  }
+  static async deletaNivel(req, res) {
+    const { id } = req.params;
+    try {
+      await database.Niveis.destroy({ where: { id: Number(id) } });
+      return res.status(200).json({ mensage: `id ${id} deletado` });
+    } catch (error) {
+      return res.status(500).json(erro.message);
+    }
+  }
+}
+
+module.exports = NivelController;
--- api/routes/index.js
+++ api/routes/index.js
@@ -1,8 +1,10 @@
 const bodyParser = require("body-parser");
 const pessoas = require("./pessoasRoute");
+const niveis = require("./niveisRoute");
+const turmas = require("./turmasRoute");
 
 module.exports = (app) => {
-  app.use(bodyParser.json());
-  app.use(pessoas);
+  app.use(bodyParser.json(), pessoas, niveis, turmas);
+
   app.get("/", (req, res) => res.send("Olá"));
 };
