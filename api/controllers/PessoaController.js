6aa94e0da54300a91b3ca01126251fcee8f71de2
matheusrodacki <44122620+matheusrodacki@users.noreply.github.com>
2023-09-15 01:27:27 -0300

    feat: Refactor PessoaController and add matricula routes
    - Refactored the `pegaUmaPessoa` method in `PessoaController` to use `umaPessoa` instead of `pessoa` for better readability.
    - Added `pegaUmaMatricula`, `criaMatricula`, `atualizaMatricula`, and `apagaMatricula` methods to `PessoaController` for managing matriculas.
    - Updated the routes in `pessoasRoute.js` to include the new matricula routes.
    
    This commit addresses the above changes.

--- api/controllers/PessoaController.js
+++ api/controllers/PessoaController.js
@@ -12,12 +12,12 @@ class PessoaController {
   static async pegaUmaPessoa(req, res) {
     const { id } = req.params;
     try {
-      const pessoa = await database.Pessoas.findOne({
+      const umaPessoa = await database.Pessoas.findOne({
         where: {
           id: Number(id),
         },
       });
-      return res.status(200).json(pessoa);
+      return res.status(200).json(umaPessoa);
     } catch (error) {
       return res.status(500).json(erro.message);
     }
@@ -51,6 +51,60 @@ class PessoaController {
       return res.status(200).json({ mensage: `id ${id} deletado` });
     } catch (error) {
       return res.status(500).json(erro.message);
+    }
+  }
+  static async pegaUmaMatricula(req, res) {
+    const { estudanteId, matriculaId } = req.params;
+    try {
+      const umaMatricula = await database.Matriculas.findOne({
+        where: {
+          id: Number(matriculaId),
+          estudante_id: Number(estudanteId),
+        },
+      });
+      return res.status(200).json(umaMatricula);
+    } catch (error) {
+      return res.status(500).json(erro.message);
+    }
+  }
+  static async criaMatricula(req, res) {
+    const { estudanteId } = req.params;
+    const novaMatricula = { ...req.body, estudante_id: Number(estudanteId) };
+    try {
+      const novaMatriculaCriada = await database.Matriculas.create(
+        novaMatricula
+      );
+      return res.status(200).json(novaMatriculaCriada);
+    } catch (error) {
+      return res.status(500).json(erro.message);
+    }
+  }
+  static async atualizaMatricula(req, res) {
+    const { estudanteId, matriculaId } = req.params;
+    const novasInfos = req.body;
+    try {
+      await database.Matriculas.update(novasInfos, {
+        where: {
+          id: Number(matriculaId),
+          estudante_id: Number(estudanteId),
+        },
+      });
+      const MatriculaAtualizada = await database.Matriculas.findOne({
+        where: { id: Number(matriculaId) },
+      });
+      return res.status(200).json(MatriculaAtualizada);
+    } catch (error) {
+      return res.status(500).json(error.message);
+    }
+  }
+
+  static async apagaMatricula(req, res) {
+    const { estudanteId, matriculaId } = req.params;
+    try {
+      await database.Matriculas.destroy({ where: { id: Number(matriculaId) } });
+      return res.status(200).json({ mensagem: `id ${matriculaId} deletado` });
+    } catch (error) {
+      return res.status(500).json(error.message);
     }
   }
 }
