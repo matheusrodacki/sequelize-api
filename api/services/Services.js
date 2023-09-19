8def98c8ef6971145749df543f4f5510e9694cde
matheusrodacki <44122620+matheusrodacki@users.noreply.github.com>
2023-09-19 12:02:56 -0300

    feat: Add Services class for fetching data dynamically
    This commit introduces a new Services class that allows fetching data dynamically based on the provided model name. The class is added in the Services.js file and includes a constructor and a method, pegaTodosOsRegistros, which retrieves all records from the specified model. This functionality is utilized in the PessoaController.js file, where instead of directly calling the database.Pessoas.findAll() method, the new services instance is used to fetch all active personnes.

---
+++ api/services/Services.js
@@ -0,0 +1,13 @@
+const database = require("../models");
+
+class Services {
+  constructor(nomeDoModelo) {
+    this.nomeDoModelo = nomeDoModelo;
+  }
+
+  async pegaTodosOsRegistros() {
+    return await database[this.nomeDoModelo].findAll();
+  }
+}
+
+module.exports = Services;
